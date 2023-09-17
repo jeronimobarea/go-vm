c3acc01e940593a3a9a3c6f3fc998d1ab0eff845
jeronimobarea-wsl <jeronimobarealucas@gmail.com>
2023-09-17 20:03:45 +0200

    feat: wip

--- cmd/vm/main.go
+++ cmd/vm/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/ethereum/go-ethereum/ethdb/leveldb"
+	"github.com/ethereum/go-ethereum/trie"
+)
+
 func main() {
-	
+	db, err := leveldb.New("", 0, 0, "", false)
+	if err != nil {
+		panic(err)
+	}
+	trie.New(nil, db)
+	trie.NewDatabase(db, nil)
 }
--- internal/execution/execution.go
+++ internal/execution/execution.go
@@ -3,15 +3,42 @@ package execution
 import (
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/trie"
+
 	"github.com/jeronimobarea/go-vm/internal/memory"
 	"github.com/jeronimobarea/go-vm/internal/stack"
 )
 
-type Execution struct {
-	code           [8]uint
-	stack          stack.Stack
-	memory         memory.Memory
-	programCounter uint
-	stopped        bool
-	output         *big.Int
+type ExecutionContext struct {
+	code            [8]uint
+	stack           *stack.Stack
+	memory          *memory.Memory
+	programCounter  uint
+	stopped         bool
+	output          *big.Int
+	storage         *trie.Trie
+	originalStorage *trie.Trie
+	gas             *big.Int
+}
+
+func NewExecutionContext(
+	code string,
+	storage *trie.Trie,
+	gas *big.Int,
+) (*ExecutionContext, error) {
+	decodedCode, err := hexutil.DecodeBig(code)
+	if err != nil {
+		return nil, err
+	}
+	return &ExecutionContext{
+		code:            cod,
+		stack:           stack.NewStack(1024),
+		memory:          memory.NewMemory(),
+		programCounter:  0,
+		stopped:         false,
+		storage:         storage,
+		originalStorage: storage.Copy(),
+		gas:             gas,
+	}
 }
